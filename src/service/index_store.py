474ed5d37677206bf41d20cbb91e62ae2e8684f7
Jerrick Godwin <jerrick.20211156@iit.ac.lk>
2025-01-25 03:04:22 +0530

    Fixed the threshold based similarity checker returning contradicting results

--- src/service/index_store.py
+++ src/service/index_store.py
@@ -1,9 +1,9 @@
 from typing import List
+
+import faiss
 import numpy as np
 import torch
 import torch.nn as nn
-import faiss
-from torch import Tensor
 
 
 class IndexStore:
@@ -50,21 +50,17 @@ class IndexStore:
         else:
             self.embeddings = np.vstack((self.embeddings, vectors))
 
-    async def get_top_k_similar_embeddings(
+    async def search_by_top_k(
             self,
-            labels: List[str],
-            embeddings: Tensor,
             query_embedding: np.ndarray,
             top_k: int,
             return_embeddings: bool = False
     ):
         """
         Computes cosine similarity between a query embedding and a list of embeddings,
-        then returns the top-k embeddings sorted by similarity score.
+        Then returns the top-k embeddings sorted by similarity score.
 
         Args:
-            labels (list): A list of labels corresponding to the embeddings.
-            embeddings (list): A list of embeddings (numpy arrays or tensors).
             query_embedding (numpy.ndarray or list): The query embedding vector.
             top_k (int): The number of top similar embeddings to return.
             return_embeddings (bool): Whether to include embeddings in the result.
@@ -72,6 +68,27 @@ class IndexStore:
         Returns:
             list: A list of dictionaries containing 'label', 'score', and optionally 'embeddings' keys.
         """
+        labels, embeddings = [], []
+
+        # Search using HNSW
+        _, indices = self.index.search(query_embedding, top_k)
+
+        # Reshape the indices to 1D
+        indices = indices.reshape(-1)
+
+        for idx in indices:
+            label = self.labels[idx]
+            embedding = self.embeddings[idx]
+
+            # Reshape to (1, -1)
+            embedding = torch.tensor(embedding, dtype=torch.float32).reshape(1, -1)
+
+            labels.append(label)
+            embeddings.append(embedding)
+
+        # Stack the embeddings
+        embeddings = torch.cat(embeddings, dim=0)
+
         # Convert query_embedding to tensors
         query_tensor = torch.tensor(query_embedding, dtype=torch.float32)
         if query_tensor.dim() == 1:
@@ -99,19 +116,15 @@ class IndexStore:
 
     async def search_by_threshold(
             self,
-            query: np.ndarray,
-            initial_k: int = 1e+60,
+            query_embedding: np.ndarray,
             return_embeddings: bool = False
     ):
         """
         Perform an asynchronous search based on a similarity threshold.
 
         Args:
-            query (np.ndarray):
+            query_embedding (np.ndarray):
                 A NumPy array representing the query vector used to search for similar items.
-            initial_k (int, optional):
-                The initial number of top results to retrieve before applying the threshold filter.
-                Defaults to 1000.
             return_embeddings (bool, optional):
                 If True, includes the embeddings of the retrieved items in the results.
                 Defaults to False.
@@ -131,71 +144,45 @@ class IndexStore:
         assert self.index is not None, "Index has not been initialized"
 
         # Search using HNSW
-        distances, indices = self.index.search(query, initial_k)
+        distances, indices = self.index.search(query_embedding, len(self.labels))
 
-        results = []
-        for i in range(len(query)):
-            label_list, embedding_list, distance_scores = [], [], []
-            query_embedding = torch.tensor(query[i])
-            for idx in indices[i]:
-                label = self.labels[idx]
-                embedding = torch.tensor(
-                    self.embeddings[idx],
-                    dtype=torch.float32
-                ).reshape(1, -1)
+        # Reshape the result to 1D
+        distances = distances.reshape(-1)
+        indices = indices.reshape(-1)
 
-                # Compute the distance score
-                distance = distances[i][idx]
-                if distance != -1:
-                    distance = 1 / (1 + distance)
+        # Convert the query to a tensor
+        query_embedding = torch.tensor(query_embedding, dtype=torch.float32)
+        if query_embedding.dim() == 1:
+            query_embedding = query_embedding.unsqueeze(0)
 
-                    # Update the list
-                    label_list.append(label)
-                    embedding_list.append(embedding)
-                    distance_scores.append(distance)
+        results, scores = [], []
+        for idx in indices:
+            # Compute the distance score
+            distance = distances[idx]
+            if distance != -1:
+                embedding = torch.tensor(self.embeddings[idx], dtype=torch.float32).reshape(1, -1)
+                score = self.cos(query_embedding, embedding).item()
+                scores.append(score)
+                if return_embeddings:
+                    results.append({
+                        'label': self.labels[idx],
+                        'embeddings': embedding,
+                        'score': score
+                    })
+                else:
+                    results.append({
+                        'label': self.labels[idx],
+                        'score': score
+                    })
 
-            # Stack the embeddings
-            embedding_list = torch.cat(embedding_list, dim=0)
+        # Compute the threshold
+        threshold = np.mean(scores)
 
-            # Compute cosine similarity
-            cs_similarity_scores = self.cos(query_embedding, embedding_list)
+        # Filter out results where the score is below the threshold
+        results = [entry for entry in results if entry['score'] >= threshold]
 
-            # Compute the mean scores of both cs and HNSW
-            updated_scores = [
-                (cosine_score.item() + (1 / (1 + distance_score))) / 2
-                for cosine_score, distance_score in zip(cs_similarity_scores, distance_scores)
-            ]
-
-            # Compute the threshold
-            threshold = np.mean(updated_scores)
-
-            for idx in indices[i]:
-                # Get the HNSW distance score
-                distance = distances[i][idx]
-                if distance != -1:
-                    embedding = torch.tensor(
-                        self.embeddings[idx],
-                        dtype=torch.float32
-                    ).reshape(1, -1)
-
-                    # Compute score from the hnsw distance score
-                    distance = 1 / (1 + distance)
-
-                    # Get the cosine similarity score
-                    cs_score = self.cos(query_embedding, embedding).item()
-
-                    # Compute the overall score
-                    score = (distance + cs_score) / 2.0
-
-                    if score >= threshold:
-                        label = self.labels[idx]
-                        entry = {'label': label, 'score': score}
-                        if return_embeddings:
-                            entry['embeddings'] = embedding.reshape(1, -1)
-                        results.append(entry)
-
-            # Sort the results
-            return sorted(results, key=lambda x: x['score'], reverse=True) if results else []
+        # Sort the results
+        return sorted(results, key=lambda x: x['score'], reverse=True) if results else []
 
     async def search(
             self,
@@ -205,40 +192,22 @@ class IndexStore:
     ):
         assert self.index is not None, "Index has not been initialized"
 
-        # Search using HNSW
-        distances, indices = self.index.search(query_embeddings, top_k)
-
         results = []
         for i in range(len(query_embeddings)):
-            labels, embeddings = [], []
-            for idx in indices[i]:
-                label = self.labels[idx]
-                embedding = self.embeddings[idx]
-
-                # Reshape to (1, -1)
-                embedding = torch.tensor(embedding, dtype=torch.float32).reshape(1, -1)
-
-                labels.append(label)
-                embeddings.append(embedding)
-
-            # Stack the embeddings
-            embeddings = torch.cat(embeddings, dim=0)
-
-            # Get the top-k results for each query
-            query_embedding: np.ndarray = query_embeddings[i]
+            # Get the current query
+            query_embedding: np.ndarray = query_embeddings[i].reshape(1, -1)
 
             # Based on the top-k, determine the type of search
             if top_k:
-                current_results = await self.get_top_k_similar_embeddings(
-                    labels=labels,
-                    embeddings=embeddings,
+                # Rank using cosine similarity
+                current_results = await self.search_by_top_k(
                     query_embedding=query_embedding,
                     top_k=top_k,
                     return_embeddings=return_embeddings
                 )
             else:
                 current_results = await self.search_by_threshold(
-                    query=query_embedding,
+                    query_embedding=query_embedding,
                     return_embeddings=return_embeddings
                 )
             results.extend(current_results)
